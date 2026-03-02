39674b8831e43b7f395404b87a312c522df1090b
Ghost <ghost@ghst.tools>
2026-03-01 21:18:00 -0600

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import embeds from 'astro-embed/integration';
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
+    integrations: [embeds(), mdx()],
     site: "https://lore.ghst.tools",
     trailingSlash: "always",
     image: {
