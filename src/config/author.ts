8bb6ba66b2402c5af5fd7b12ae19a53ec5a48303
Ghost <ghost@ghst.tools>
2026-07-12 09:57:55 -0500

    Update old links

--- src/config/author.ts
+++ src/config/author.ts
@@ -3,9 +3,9 @@ export const AUTHOR = {
   title: "Spectral Developer",
   avatar: "https://ghst.tools/media/lore/v2/icon.png",
   socials: [
-    { label: "X",       href: "https://twitter.ghst.tools",           icon: "x"       },
-    { label: "Bluesky", href: "https://bsky.app/profile/ghst.tools",  icon: "bluesky" },
-    { label: "GitHub",  href: "https://github.ghst.tools",            icon: "github"  },
-    { label: "ko-fi",   href: "https://kofi.ghst.tools",             icon: "ko-fi"    },
+    { label: "X",       href: "https://ghst.tools/boo/x",             icon: "x"       },
+    { label: "Bluesky", href: "https://ghst.tools/boo/bsky",          icon: "bluesky" },
+    { label: "GitHub",  href: "https://ghst.tools/boo/github",        icon: "github"  },
+    { label: "ko-fi",   href: "https://ghst.tools/boo/kofi",          icon: "ko-fi"    },
   ],
 } as const;
